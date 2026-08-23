87231ea066d3181157e1ad76827899c986f563ec
Pedro Valladares <pedrohvalladares@hotmail.com>
2026-08-23 18:10:57 -0300

    feat(socket): adiciona interface para escrita direta via socket

---
+++ Koba.Infrastructure/Socket/Interfaces/IRawSocket.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Channels;
+
+namespace Koba.Infrastructure.Socket.Interfaces;
+
+internal interface IRawSocket
+{
+    Task SendAsync(string message, CancellationToken cancellationToken);
+    ChannelWriter<string> GetOutputChannel();
+    event Func<OnEventReceivedData, Task> OnEventReceived;
+}
