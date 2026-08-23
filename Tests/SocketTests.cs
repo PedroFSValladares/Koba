f6dc4ea4c52c478bb53f47ed968003c82d6c0b57
Pedro Valladares <pedrohvalladares@hotmail.com>
2026-08-23 14:29:42 -0300

    refactor(tests): adiciona arquivos de configuração para testes

--- Tests/SocketTests.cs
+++ Tests/SocketTests.cs
@@ -1,14 +1,20 @@
-﻿using Xunit.Abstractions;
+﻿using Microsoft.Extensions.Configuration;
+using Xunit.Abstractions;
 
 namespace Tests;
 
 public class SocketTests
 {
     private readonly ITestOutputHelper testOutputHelper;
+    private readonly IConfiguration config;
 
     public SocketTests(ITestOutputHelper testOutputHelper)
     {
         this.testOutputHelper = testOutputHelper;
+        config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
     }
 
     [Fact]
@@ -17,18 +23,20 @@ public class SocketTests
         var client = Utils.GetGatewayClient();
         var receiveToken = new CancellationTokenSource();
 
-        string host = "wss://echo.websocket.org";
-        string message = "ping";
+        string host = config["SocketTestConfig:EchoServerHost"];
+        string message = config["SocketTestConfig:Message"];
+        int skipMessages = Convert.ToInt32(config["SocketTestConfig:ResponsesToSkip"]);
+        int testTimeout = Convert.ToInt32(config["SocketTestConfig:Timeout"]);
         string result = "";
 
         var channels = await client.ConnectAsync(host, receiveToken.Token);
 
         try
         {
-            var sendToken = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            var sendToken = new CancellationTokenSource(TimeSpan.FromSeconds(testTimeout));
             await channels.SendMessageAsync(message, sendToken.Token);
-            
-            if(host == "wss://echo.websocket.org")
+
+            for (int i = 0; i < skipMessages; i++)
                 _ = await channels.GetMessageAsync(receiveToken.Token);
             
             result = await channels.GetMessageAsync(receiveToken.Token);
