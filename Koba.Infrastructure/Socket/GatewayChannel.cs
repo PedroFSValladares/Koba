6972be5e6bd1939de7d9e46535e78df4b9e1cbd7
Pedro Valladares <pedrohvalladares@hotmail.com>
2026-08-22 23:17:37 -0300

    refactor(socket-channel): adiciona métodos para acesso e escrita de mensagens em canais

--- Koba.Infrastructure/Socket/GatewayChannel.cs
+++ Koba.Infrastructure/Socket/GatewayChannel.cs
@@ -4,8 +4,8 @@ namespace Koba.Infrastructure.Socket;
 
 public class GatewayChannel
 {
-    public ChannelReader<string> gatewayOutput { get; private set; }
-    public ChannelWriter<string> gatewayInput { get; private set; }
+    private ChannelReader<string> gatewayOutput { get; set; }
+    private ChannelWriter<string> gatewayInput { get; set; }
 
     public static GatewayChannel Create(ChannelWriter<string> gatewayInput, ChannelReader<string> gatewayOutput)
     {
@@ -16,6 +16,40 @@ public class GatewayChannel
         
         return channel;
     }
-        
     
+    public async Task<string> GetMessageAsync() => await gatewayOutput.ReadAsync();
+    
+    public async Task<string> GetMessageAsync(CancellationToken token) => await gatewayOutput.ReadAsync(token);
+    
+
+    public async Task<IEnumerable<string>> GetAllMessagesAsync()
+    {
+        List<string> messages = [];
+        await foreach (var message in gatewayOutput.ReadAllAsync())
+        {
+            messages.Add(message);
+        }
+        return messages;
+    }
+    
+    public async Task<IEnumerable<string>> GetAllMessagesAsync(CancellationToken token)
+    {
+        List<string> messages = [];
+        await foreach (var message in gatewayOutput.ReadAllAsync(token))
+        {
+            messages.Add(message);
+        }
+        return messages;
+    }
+    
+    public async Task SendMessageAsync(string message) => await gatewayInput.WriteAsync(message);
+    
+    public async Task SendMessageAsync(string message, CancellationToken token) => await gatewayInput.WriteAsync(message, token);
+    
+        
+    public void Complete()
+    {
+        gatewayInput.Complete();
+        gatewayInput.Complete();
+    }
 }
