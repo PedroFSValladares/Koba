92169788f58a251624505338b6276d56beeca561
Pedro Valladares <pedrohvalladares@hotmail.com>
2026-08-22 23:35:26 -0300

    feat(tests): adiciona teste de integração para websocket

---
+++ Tests/SocketTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit.Abstractions;
+
+namespace Tests;
+
+public class SocketTests
+{
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public SocketTests(ITestOutputHelper testOutputHelper)
+    {
+        this.testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public async Task SocketClient_Deve_Receber_e_Enviar_Mensagem_Sucesso()
+    {
+        var client = Utils.GetGatewayClient();
+        var receiveToken = new CancellationTokenSource();
+
+        string host = "wss://echo.websocket.org";
+        string message = "ping";
+        string result = "";
+
+        var channels = await client.ConnectAsync(host, receiveToken.Token);
+
+        try
+        {
+            var sendToken = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            await channels.SendMessageAsync(message, sendToken.Token);
+            
+            if(host == "wss://echo.websocket.org")
+                _ = await channels.GetMessageAsync(receiveToken.Token);
+            
+            result = await channels.GetMessageAsync(receiveToken.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            Assert.Fail("Timeout exceeded.");
+        }
+        
+        Assert.Equal(message, result);
+        
+        await receiveToken.CancelAsync();
+    }
+}
---
+++ Tests/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Channels;
+using Koba.Infrastructure.Socket;
+using Koba.Infrastructure.Socket.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Tests;
+
+public static class Utils
+{
+    public static IGatewayClient GetGatewayClient()
+    {
+        var webSocket = new System.Net.WebSockets.ClientWebSocket();
+        Channel<string> channel1 = Channel.CreateUnbounded<string>();
+        Channel<string> channel2 = Channel.CreateUnbounded<string>();
+        var logger = new Mock<ILogger<SocketClient>>();
+        
+        return new SocketClient(webSocket, channel1, channel2, logger.Object);
+    }
+}
