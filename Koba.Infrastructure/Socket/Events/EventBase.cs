1f67e44fcf22a6048c478cb63ff383173dab7480
Pedro Valladares <pedrohvalladares@hotmail.com>
2026-08-23 18:09:55 -0300

    feat(socket-manager): adiciona abstração para alguns eventos do gateway do discord

---
+++ Koba.Infrastructure/Socket/Events/EventBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Koba.Infrastructure.Socket.Events;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "op")]
+[JsonDerivedType(typeof(HelloEvent), typeDiscriminator: 10)]
+[JsonDerivedType(typeof(HeartBeatEvent), typeDiscriminator: 1)]
+[JsonDerivedType(typeof(HeartBeatAckEvent), typeDiscriminator: 11)]
+internal abstract record EventBase(int op, int? s, string? t);
+
+internal abstract record HelloEvent(int op, HelloData d) : EventBase(op, null, null);
+internal abstract record HeartBeatEvent(int op, int d)  : EventBase(op, null , null);
+internal abstract record HeartBeatAckEvent(int op) : EventBase(op, null, null);
---
+++ Koba.Infrastructure/Socket/Events/Hello.cs
@@ -0,0 +1,6 @@
+﻿namespace Koba.Infrastructure.Socket.Events;
+
+internal class HelloData
+{
+    internal int heartbeat_interval;
+}
