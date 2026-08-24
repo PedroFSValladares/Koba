14c8496898fb32fa5e555ffe7551c673e09b26f6
Pedro Valladares <pedrohvalladares@hotmail.com>
2026-08-23 21:09:58 -0300

    refactor(socket-events): unifica eventos num arquivos só e adiciona passagem do campo s

---
+++ Koba.Infrastructure/Socket/Events/Events.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Koba.Infrastructure.Socket.Events;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "op")]
+[JsonDerivedType(typeof(HelloEvent), typeDiscriminator: 10)]
+[JsonDerivedType(typeof(HeartBeatEvent), typeDiscriminator: 1)]
+[JsonDerivedType(typeof(HeartBeatAckEvent), typeDiscriminator: 11)]
+[JsonDerivedType(typeof(ReconnectEvent), typeDiscriminator: 7)]
+internal abstract record EventBase(int op, int? s, string? t);
+
+internal abstract record HelloEvent(int op, HelloData d, int? s, string? t) : EventBase(op, s, t);
+internal record HeartBeatEvent(int op, int d, int? s, string? t)  : EventBase(op, s , t);
+internal abstract record HeartBeatAckEvent(int op, int? s, string? t) : EventBase(op, s, t);
+internal abstract record ReconnectEvent(int op, int? s, string? t) : EventBase(op, s, t);
+internal abstract record InvalidSessionEvent(int op, bool d, int? s, string? t) : EventBase(op, s, t);
+
+internal abstract record HelloData(int heartbeat_interval); 
+internal record ResumeData(string token, string session_id, int seq);
--- Koba.Infrastructure/Socket/Events/Hello.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Koba.Infrastructure.Socket.Events;
-
-internal class HelloData
-{
-    internal int heartbeat_interval;
-}
